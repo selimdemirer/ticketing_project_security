63bc5e4db9879bb1eb39c5b09bb56797021a3db3
selimdemirer <selimmdemirerr@gmail.com>
2023-10-15 11:21:06 +0300

    I don't want to use hard coded / manuel users. I want to use a user which I don't know where it is coming from. (I didn't connect it to repo yet) For this I created common package and UserPrincipal class. Since Spring doesn't understand my users, I need to map my users to UserDetailsService by using UserPrincipal

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -18,22 +18,22 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(
-                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
-        );
-
-        userList.add(
-                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
-        );
-
-        return new InMemoryUserDetailsManager(userList);
-
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(
+//                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
+//        );
+//
+//        userList.add(
+//                new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
+//        );
+//
+//        return new InMemoryUserDetailsManager(userList);
+//
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -41,12 +41,13 @@ public class SecurityConfig {
 
         return http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasRole("ADMIN") // ROLE_ is automatically added
-                .antMatchers("/project/**").hasRole("MANAGER")
-                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
-                .antMatchers("/task/**").hasRole("MANAGER")
-//              .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
-                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+//                .antMatchers("/user/**").hasRole("ADMIN") // ROLE_ is automatically added
+                .antMatchers("/user/**").hasAuthority("ROLE_ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+//                .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
+//                .antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
                 .antMatchers(
                         "/",
                         "/login",
---
+++ src/main/java/com/cydeo/service/SecurityService.java
@@ -0,0 +1,9 @@
+package com.cydeo.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityService extends UserDetailsService {
+
+
+
+}
